e9c03230185bd3eac0956d65d906e295f2d1c3a6
Ernest Rhinozeros <coding+github@brainhash.de>
2017-11-10 11:57:36 +0100

    adds basic register page

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+type Container = 'container' | 'container-fluid';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  public container: Container = 'container';
 }
